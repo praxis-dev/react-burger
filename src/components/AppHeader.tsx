a5175559db0567d35806aa0eedfaf406f6a18de7
I <chesnokovprm@gmail.com>
2022-12-16 15:54:32 +0300

    formatter configured for React

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,21 +1,30 @@
-import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import {
+  value Logo,
+  value BurgerIcon,
+  value ListIcon,
+  value ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 
 function AppHeader() {
-    return (
-      <div className="App">
-        <header>
-            <div className="header__icon-with-text"><BurgerIcon type="primary"/><p>Конструктор</p></div>
-        <div className="header__icon-with-text"><ListIcon type="primary"/><div className="p">Лента заказов</div>
-</div>
-<Logo />
-<div className="header__icon-with-text">
-<ProfileIcon type="primary"/> 
-<p>Личный кабинет</p>
-</div>
-        </header>
-      </div>
-    );
-  }
-  
-  export default AppHeader;
-  
+  return (
+    <div className="App">
+      <header>
+        <div className="header__icon-with-text">
+          <BurgerIcon type="primary" />
+          <p>Конструктор</p>
+        </div>
+        <div className="header__icon-with-text">
+          <ListIcon type="primary" />
+          <div className="p">Лента заказов</div>
+        </div>
+        <Logo />
+        <div className="header__icon-with-text">
+          <ProfileIcon type="primary" />
+          <p>Личный кабинет</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default AppHeader;
