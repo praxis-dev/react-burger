2549c937ce092bbd1103b5048ae2acb6ae80e3e6
I <chesnokovprm@gmail.com>
2022-12-18 20:10:07 +0300

    constructor layout progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./App.css";
 import AppHeader from "./components/AppHeader/AppHeader";
 import BurgerConstructor from "./components/BurgerConstructor/BurgerConstructor";
--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -1,8 +1,42 @@
 import {} from "@ya.praktikum/react-developer-burger-ui-components";
+
 import css from "./BurgerConstructor.module.css";
 
 function BurgerConstructor() {
-  return <div className="section"></div>;
+  const apiGet = () => {
+    fetch("https://norma.nomoreparties.space/api/ingredients").then(
+      (response) => response.json().then((json) => console.log(json))
+    );
+  };
+
+  const clickHandler = () => {
+    return (event: React.MouseEvent) => {
+      event.preventDefault();
+      console.log("click");
+      apiGet();
+    };
+  };
+
+  return (
+    <>
+      <div className={css.section}>
+        <div className={css.first}>
+          {" "}
+          <h1 className={css.header}>Соберите бургер</h1>
+        </div>
+        <div className={css.componentsandorder}>
+          <div className={css.components}>
+            <div className={css.selectorcontainer}>
+              <div className={css.componentsselector}>Булки</div>
+              <div className={css.componentsselector}>Соусы</div>
+              <div className={css.componentsselector}>Начинки</div>
+            </div>
+          </div>
+          <div className={css.order}>testestes</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default BurgerConstructor;
