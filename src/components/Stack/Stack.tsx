10a8a1e9bcb276b26a6b32fb3a84852436a0ba12
I <chesnokovprm@gmail.com>
2022-12-20 17:38:45 +0300

    total layout progress

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,14 +1,17 @@
 import css from "./Stack.module.css";
-
 import imageThumbnail from "../../images/sauce-03.png";
-
 import StackComponent from "../StackComponent/StackComponent";
+import largeCurrencyIcon from "../../images/Subtract.svg";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 
 function Stack() {
   return (
     <>
       {" "}
-      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <div
+        className={css.stack}
+        style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+      >
         <StackComponent
           type="top"
           isLocked={true}
@@ -28,6 +31,20 @@ function Stack() {
           price={50}
           thumbnail={imageThumbnail}
         />
+      </div>
+      <div className={css.buttonAndTotalContainer}>
+        <div className={css.totalAndIcon}>
+          <p className={css.total}>7777</p>
+          <img
+            className={css.largeCurrencyIcon}
+            src={largeCurrencyIcon}
+            alt="Иконка галактической валюты крупная"
+          />
+        </div>
+        <Button htmlType="button" type="primary" size="medium">
+          {" "}
+          Оформить заказ{" "}
+        </Button>
       </div>
     </>
   );
