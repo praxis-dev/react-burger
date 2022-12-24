135739cbd4384d8c10c20f9ab41bff7056fe66bb
I <chesnokovprm@gmail.com>
2022-12-24 05:20:21 +0300

    1st feedback stack progress

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,8 +1,9 @@
 import css from "./Stack.module.css";
-import imageThumbnail from "../../images/sauce-03.png";
+import fixedElement from "../../images/bun-02.png";
 import StackComponent from "../StackComponent/StackComponent";
 import largeCurrencyIcon from "../../images/Subtract.svg";
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import filler from "../../images/sauce-03.png";
 
 function Stack() {
   return (
@@ -11,48 +12,28 @@ function Stack() {
         <StackComponent
           type="top"
           isLocked={true}
-          text="Соус традиционный галактический"
-          price={50}
-          thumbnail={imageThumbnail}
+          text="Краторная булка N-200i (верх)"
+          price={20}
+          thumbnail={fixedElement}
         />
         <div className={css.stackScreen}>
           <StackComponent
             text="Соус традиционный галактический"
             price={50}
-            thumbnail={imageThumbnail}
+            thumbnail={filler}
           />
           <StackComponent
             text="Соус традиционный галактический"
             price={50}
-            thumbnail={imageThumbnail}
-          />
-          <StackComponent
-            text="Соус традиционный галактический"
-            price={50}
-            thumbnail={imageThumbnail}
-          />
-          <StackComponent
-            text="Соус традиционный галактический"
-            price={50}
-            thumbnail={imageThumbnail}
-          />
-          <StackComponent
-            text="Соус традиционный галактический"
-            price={50}
-            thumbnail={imageThumbnail}
-          />
-          <StackComponent
-            text="Соус традиционный галактический"
-            price={50}
-            thumbnail={imageThumbnail}
+            thumbnail={filler}
           />
         </div>
         <StackComponent
           type="bottom"
           isLocked={true}
-          text="Соус традиционный галактический"
-          price={50}
-          thumbnail={imageThumbnail}
+          text="Краторная булка N-200i (низ)"
+          price={20}
+          thumbnail={fixedElement}
         />
       </div>
       <div className={css.buttonAndTotalContainer}>
