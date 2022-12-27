6af2d6d0fbb36ffa8225095263e4af80a80a42cd
I <chesnokovprm@gmail.com>
2022-12-27 20:43:48 +0300

    1st modal content progress

--- src/components/IngredientCard/IngredientCard.tsx
+++ src/components/IngredientCard/IngredientCard.tsx
@@ -1,4 +1,6 @@
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+// import { Popup } from "../Popup/Popup";
+import { useState } from "react";
 
 import css from "./IngredientCard.module.css";
 
@@ -35,13 +37,21 @@ function IngredientCard(props: props) {
     __v,
   } = props;
 
-  function clickHandler(e: any) {
-    console.log("clickHandler");
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
   }
 
   return (
     <>
-      <div onClick={clickHandler} className={css.component}>
+      <div onClick={toggleModal} className={css.component}>
         <div className={css.portionsCounter}>2</div>
         <img
           alt="Иконка компонента"
@@ -55,6 +65,25 @@ function IngredientCard(props: props) {
         </div>
         <p className={css.componentName}>{name}</p>
       </div>
+      {modal && (
+        <div className={css.modal}>
+          <div onClick={toggleModal} className={css.overlay}></div>
+          <div className={css.modalContent}>
+            <h2 className={css.orderIdentifier}>7777</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+              perferendis suscipit officia recusandae, eveniet quaerat assumenda
+              id fugit, dignissimos maxime non natus placeat illo iusto!
+              Sapiente dolorum id maiores dolores? Illum pariatur possimus
+              quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
+              placeat tempora vitae enim incidunt porro fuga ea.
+            </p>
+            <button className={css.closeModal} onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -2,12 +2,10 @@ import {} from "@ya.praktikum/react-developer-burger-ui-components";
 
 import css from "./Popup.module.css";
 
-function Popup() {
+export function Popup() {
   return (
     <>
       <div className={css.popup}></div>
     </>
   );
 }
-
-export default Popup;
