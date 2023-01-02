81ea88a32a45e1e0189c94ddf103518b17d2d5ce
praxis-dev <chesnokovprm@gmail.com>
2023-01-02 11:22:04 +0300

    1st feedback unnecessary fragment from OrderDetails removed  12/18

--- src/components/OrderDetails/OrderDetails.tsx
+++ src/components/OrderDetails/OrderDetails.tsx
@@ -8,20 +8,18 @@ function OrderDetails(props: props) {
   const { toggleModal } = props;
 
   return (
-    <>
-      <div className={css.modalContent}>
-        <h2 className={css.orderIdentifier}>7777</h2>
-        <p className={css.orderIdentifierText}>идентификатор заказа</p>
-        <img src={done} alt="Иконка заказа" className={css.doneIcon} />
-        <p className={css.orderText}>Ваш заказ начали готовить</p>
-        <p className={css.awaitText}>
-          Дождитесь готовности на орбитальной станции
-        </p>
-        <div className={css.closeModal} onClick={toggleModal}>
-          <CloseIcon type="primary" />
-        </div>
+    <div className={css.modalContent}>
+      <h2 className={css.orderIdentifier}>7777</h2>
+      <p className={css.orderIdentifierText}>идентификатор заказа</p>
+      <img src={done} alt="Иконка заказа" className={css.doneIcon} />
+      <p className={css.orderText}>Ваш заказ начали готовить</p>
+      <p className={css.awaitText}>
+        Дождитесь готовности на орбитальной станции
+      </p>
+      <div className={css.closeModal} onClick={toggleModal}>
+        <CloseIcon type="primary" />
       </div>
-    </>
+    </div>
   );
 }
 
