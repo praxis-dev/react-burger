c4282be24c345368b2581901dea07be0ba962e3d
praxis-dev <chesnokovprm@gmail.com>
2023-02-24 13:39:39 -0300

    cookie saved ok

--- src/services/middleware/authUserMiddleWare.tsx
+++ src/services/middleware/authUserMiddleWare.tsx
@@ -1,6 +1,31 @@
 import { ingredientsSlice } from "../slice/ingredientsSlice";
 import { authUserApi } from "../api/authUserApi";
 
+const setCookie = (name: string, value: string, options: any = {}) => {
+  options = {
+    path: "/",
+    ...options,
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie =
+    encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+  console.log(document.cookie);
+};
+
 export const authUserMiddleware = (data: any) => {
   return async function (dispatch: any) {
     dispatch(ingredientsSlice.actions._LOGIN_REQUEST("Loading"));
@@ -11,14 +36,6 @@ export const authUserMiddleware = (data: any) => {
     const res = await result.json();
     dispatch(ingredientsSlice.actions._LOGIN_SUCCESS(res));
     data.onResponse(res);
+    setCookie("token", res.refreshToken, { "max-age": 1200 });
   };
 };
-
-// dispatch(ingredientsSlice.actions._REGISTER_REQUEST("Loading"));
-// const result = await registerNewUserApi(data);
-// if (!result.ok) {
-//   dispatch(ingredientsSlice.actions._REGISTER_ERROR(result.statusText));
-// }
-// const res = await result.json();
-// dispatch(ingredientsSlice.actions._REGISTER_SUCCESS(res));
-// data.onResponse(res);
