a393bf22b3d8f3c7532fc71810300ed4ed9bb59a
praxis-dev <chesnokovprm@gmail.com>
2023-03-11 20:14:56 -0300

    chore: initial dispatch added

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -9,3 +9,7 @@ export interface updateUserThunkData {
   email: string;
   password: string;
 }
+
+export interface dispatch {
+  (arg0: { type: string; payload: any }): void;
+}
--- src/services/thunk/updateUserThunk.tsx
+++ src/services/thunk/updateUserThunk.tsx
@@ -1,9 +1,10 @@
 import { updateUserDataApi } from "../api/updateUserDataApi";
 import { ingredientsSlice } from "../slice/ingredientsSlice";
 import { updateUserThunkData } from "../../interfaces";
+import { dispatch } from "../../interfaces";
 
 export const updateUserThunk = (data: updateUserThunkData) => {
-  return async function (dispatch: any) {
+  return async function (dispatch: dispatch) {
     dispatch(ingredientsSlice.actions._UPDATE_USER_REQUEST("Loading"));
     const result = await updateUserDataApi(data);
 
