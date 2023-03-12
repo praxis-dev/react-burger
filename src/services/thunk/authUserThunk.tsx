85f1e784c1938fa2d79d49f03c5157023c85d06c
praxis-dev <chesnokovprm@gmail.com>
2023-03-12 19:16:18 -0300

    chore: try/catch added to authUserThunk

--- src/services/thunk/authUserThunk.tsx
+++ src/services/thunk/authUserThunk.tsx
@@ -6,12 +6,15 @@ import { Dispatch } from "../../interfaces";
 export const authUserThunk = (data: any) => {
   return async function (dispatch: Dispatch) {
     dispatch(ingredientsSlice.actions._LOGIN_REQUEST("Loading"));
-    const res = await authUserApi(data);
+    try {
+      const res = await authUserApi(data);
 
-    dispatch(ingredientsSlice.actions._LOGIN_SUCCESS(res));
-    data.onResponse(res);
-    setCookie("accessToken", res.accessToken, { "max-age": 1200 });
-
-    setCookie("token", res.refreshToken, { "max-age": 1200 });
+      dispatch(ingredientsSlice.actions._LOGIN_SUCCESS(res));
+      data.onResponse(res);
+      setCookie("accessToken", res.accessToken, { "max-age": 1200 });
+      setCookie("token", res.refreshToken, { "max-age": 1200 });
+    } catch (error) {
+      dispatch(ingredientsSlice.actions._LOGIN_ERROR((error as Error).message));
+    }
   };
 };
