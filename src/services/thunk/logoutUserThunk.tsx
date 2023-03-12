3f0da9dbe388c982375371726e5353ef20449e50
praxis-dev <chesnokovprm@gmail.com>
2023-03-12 19:30:40 -0300

    chore: catch added to logoutUserThunk

--- src/services/thunk/logoutUserThunk.tsx
+++ src/services/thunk/logoutUserThunk.tsx
@@ -3,9 +3,24 @@ import { deleteCookie } from "../../utils/cookies/deleteCookie";
 import { Dispatch } from "../../interfaces";
 
 export const logoutUserThunk = () => {
-  return async function (dispatch: Dispatch) {
+  return function (dispatch: Dispatch) {
     dispatch(ingredientsSlice.actions._LOGOUT_USER_REQUEST("Loading"));
     deleteCookie("token");
     deleteCookie("accessToken");
+    Promise.resolve()
+      .then(() => {
+        dispatch(ingredientsSlice.actions._LOGOUT_USER_SUCCESS());
+      })
+      .catch((error) => {
+        if (error instanceof Error) {
+          dispatch(ingredientsSlice.actions._LOGOUT_USER_ERROR(error.message));
+        } else {
+          dispatch(
+            ingredientsSlice.actions._LOGOUT_USER_ERROR(
+              "An unknown error occurred."
+            )
+          );
+        }
+      });
   };
 };
